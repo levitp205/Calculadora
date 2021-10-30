e73d628b115e8e55074272b622694ce0c0078004
Levi Provetti <levi.provetti@gmail.com>
2021-10-30 05:18:43 -0700

    # WARNING: head commit changed in the meantime
    first commit

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -7,7 +7,7 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int a, b;
+		 int a, b;
 		
 		System.out.println("Digite o primeiro valor");
 		a = scan.nextInt();
@@ -17,7 +17,7 @@ public class Calculadora {
 		int soma = soma(a,b);
 		int subtracao = subtracao(a,b);
 		int multiplicacao = multiplicacao(a,b);
-		int divisao = divisao(a,b);
+		double divisao = divisao(a,b);
 		
 		System.out.println(soma);
 		System.out.println(subtracao);
@@ -33,10 +33,10 @@ public class Calculadora {
 		return a + b;
 	}
 	
-	public static int ubtracao(int a, int b) {
+	public static int subtracao(int a, int b) {
 		return a - b;
 	}
-	public static int divisao(int a, int b) {
+	public static double divisao(double a, double b) {
 		return a / b;
 	}
 	public static int multiplicacao(int a, int b) {
